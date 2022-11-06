faeddd9c21fc0e72f6de39f77de2efde22ec51c9
Nurjaman <30688893+nurjaman24@users.noreply.github.com>
2022-11-07 02:41:49 +0700

    Update Base Url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/project/satudesa';
+$config['base_url'] = 'http://localhost/SatuDesa';
 // $config['base_url'] = 'https://meniti.id/sadesa/';
 
 
