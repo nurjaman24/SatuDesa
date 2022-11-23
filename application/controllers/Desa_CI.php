bf4bf7931f88d6078c4a8ead1847a9013d521508
Nurjaman <30688893+nurjaman24@users.noreply.github.com>
2022-11-24 03:05:59 +0700

    update

--- application/controllers/Desa_CI.php
+++ application/controllers/Desa_CI.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class desa extends CI_Controller {
+class Desa_CI extends CI_Controller {
     function __construct(){
         parent::__construct();
         // Data login
